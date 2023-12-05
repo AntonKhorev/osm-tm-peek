579fa5f7de40ec97c186d1f689588c9bc0a22f56
Anton Khorev <tony29@yandex.ru>
2023-12-05 05:14:23 +0300

    Get project id from comment

--- src/changeset.js
+++ src/changeset.js
@@ -1,5 +1,26 @@
-console.log(`injected into changeset page`)
+const $browseSection=document.querySelector('#sidebar_content > .browse-section')
+if ($browseSection) {
+	let hotosmProjectId
+	const $comment=$browseSection.querySelector(':scope > p.fst-italic')
+	if ($comment) {
+		hotosmProjectId=matchHotosmProjectId($comment.textContent)
+	}
+	const $tagsContainer=$browseSection.querySelector(':scope > .rounded')
+	if ($tagsContainer) {
+		const $tags=$browseSection.querySelector('table.browse-tag-list')
+	}
+	const $discussion=$browseSection.querySelector(':scope > .row')
+	if (hotosmProjectId!=null && $discussion) {
+		const $tmSummary=document.createElement('summary')
+		$tmSummary.textContent=`#hotosm-project-${hotosmProjectId}`
+		const $tmDetails=document.createElement('details')
+		$tmDetails.append($tmSummary)
+		$discussion.prepend($tmDetails)
+	}
+}
 
-const $test = document.createElement('div')
-$test.textContent = `Testing html injection`
-document.body.append($test)
+function matchHotosmProjectId(s) {
+	const match=s.match(/#hotosm-project-(\d+)/)
+	if (!match) return
+	return match[1]
+}
